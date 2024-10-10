6ac1a39a05e4cbea0a43612cf4bd08d7f965de96
jinnyjung-coder <yujin.jung0987@gmail.com>
2024-10-11 01:59:19 +0900

    Create lv2_condition.sql

---
+++ lv2_condition.sql
@@ -0,0 +1,13 @@
+/* https://school.programmers.co.kr/learn/courses/30/lessons/273709
+
+ITEM_INFO 테이블에서 희귀도가 'LEGEND'인 아이템들의 가격의 총합을 구하는 SQL문을 작성해 주세요. 이때 컬럼명은 'TOTAL_PRICE'로 지정해 주세요.*/
+
+#SOL1
+SELECT SUM(PRICE) AS TOTAL_PRICE
+FROM (SELECT ITEM_ID, ITEM_NAME, RARITY, PRICE
+      FROM ITEM_INFO
+      WHERE RARITY='LEGEND') AS SUBQUERY;
+
+#SOL2
+SELECT SUM(PRICE) AS TOTAL_PRICE FROM ITEM_INFO
+WHERE RARITY = 'LEGEND'
