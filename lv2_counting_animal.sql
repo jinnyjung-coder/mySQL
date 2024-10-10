122adb045407a33bc0f18ad6758a892bc83c1976
jinnyjung-coder <yujin.jung0987@gmail.com>
2024-10-11 01:46:10 +0900

    Create lv2_counting_animal.sql

---
+++ lv2_counting_animal.sql
@@ -0,0 +1,10 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/59406
+# 동물 보호소에 동물이 몇 마리 들어왔는지 조회하는 SQL 문을 작성해주세요.
+
+# SOL 1
+SELECT COUNT(ANIMAL_ID)
+FROM ANIMAL_INS
+
+# SOL 2
+SELECT COUNT(*) as count /* 테이블의 모든 행을 선택.*/
+FROM animal_ins 
