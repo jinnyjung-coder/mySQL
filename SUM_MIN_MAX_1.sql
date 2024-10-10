d81c66c45951a54d30aab8876be32c6840c70ec0
jinnyjung-coder <yujin.jung0987@gmail.com>
2024-10-11 01:16:58 +0900

    Create SUM_MIN_MAX_1.sql
    https://school.programmers.co.kr/learn/courses/30/lessons/131115

---
+++ SUM_MIN_MAX_1.sql
@@ -0,0 +1,24 @@
+# PRINT THE MOST EXPENSIVE PRICE OF FOOD INFORMATION
+# Write an SQL statement to retrieve the food ID, food name, food code, food classification, and food price of the most expensive food item in the FOOD_PRODUCT table.
+# https://school.programmers.co.kr/learn/courses/30/lessons/131115
+
+  # 단순히 SELECT MAX(PRICE) 를 쓰면 안되는 이유는, 가격만 MAX가 나오고, 다른 열의 정보는 MAX 가격과 매칭 되는 정보가 아니기 때문!
+  
+#SOL 1 (WHERE SUB-QUERY)
+SELECT PRODUCT_ID, PRODUCT_NAME, PRODUCT_CD, CATEGORY, PRICE
+FROM FOOD_PRODUCT
+WHERE PRICE = (SELECT MAX(PRICE) FROM FOOD_PRODUCT)
+
+#SOL 2 (ORDER BY, LIMIT)
+  # 가장 비싼 가격이 2개인 경우, 오답처리 될 가능성 있음
+SELECT PRODUCT_ID, PRODUCT_NAME, PRODUCT_ID, CATEGORY, PRICE
+FROM FOOD_PRODUCT
+ORDER BY PRICE DESC
+LIMIT 1
+
+#SOL 3 (JOIN, FROM QUERY)
+SELECT PRODUCT_ID, PRODUCT_NAME, PRODUCT_ID, CATEGORY, PRICE
+FROM FOOD_PRODUCT F1 JOIN (SELECT MAX(PRICE) PRICE
+  FROM FOOD_PRODUCT T) F2
+ON F1.PRICE = F2.PRICE
+
