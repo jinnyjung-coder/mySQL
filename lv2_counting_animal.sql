bce360ff4bc89437536fe14e490b24641026c2f9
jinnyjung-coder <yujin.jung0987@gmail.com>
2024-10-11 23:15:55 +0900

    Update lv2_counting_animal.sql

--- lv2_counting_animal.sql
+++ lv2_counting_animal.sql
@@ -1,5 +1,11 @@
-#https://school.programmers.co.kr/learn/courses/30/lessons/59406
-# 동물 보호소에 동물이 몇 마리 들어왔는지 조회하는 SQL 문을 작성해주세요.
+/* https://school.programmers.co.kr/learn/courses/30/lessons/59406
+동물 보호소에 동물이 몇 마리 들어왔는지 조회하는 SQL 문을 작성해주세요.
+
+COUNT(*) VS COUNT(COLUMN_NAME)
+COUNT(*): NULL 포함
+COUNT(COLUMN_NAME): NULL 값 제외한 행의 개수
+
+*/
 
 # SOL 1
 SELECT COUNT(ANIMAL_ID)
